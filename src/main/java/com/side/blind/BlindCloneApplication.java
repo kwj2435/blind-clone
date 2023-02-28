cd1188d7f93990ddac7b9d1f3775ad446481378e
kimuijin <dmlwls2435@naver.com>
2023-02-28 23:30:11 +0900

    Initial commit

---
+++ src/main/java/com/side/blind/BlindCloneApplication.java
@@ -0,0 +1,13 @@
+package com.side.blind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlindCloneApplication.class, args);
+    }
+
+}
